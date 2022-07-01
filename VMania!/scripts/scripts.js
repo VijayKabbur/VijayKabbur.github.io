4f6f33111161f191bb0bba880922165c8b009eef
Vijay Kabbur <53096977+VijayKabbur@users.noreply.github.com>
2022-07-01 18:08:56 +0530

    Added a function in script to switch sub_body

--- VMania!/scripts/scripts.js
+++ VMania!/scripts/scripts.js
@@ -1,5 +1,28 @@
+function switch_sub_body(z){
+    var sub_bodies = ["sub_body_00_menu", "sub_body_01_play_area"];
+    for(let i=0; i < sub_bodies.length; i++){
+        document.getElementById(sub_bodies[i]).style.display = "none";
+    }
+    z.forEach(y => {
+        document.getElementById(sub_bodies[y]).style.display = "block";
+    });
+}
+
+switch_sub_body([0]);
+
+function lets_play(){
+    switch_sub_body([1]);
+}
+
+function start_new_game(){
+}
+
+function back_to_menu(){
+    switch_sub_body([0]);
+}
+
 function quit_game(){
-    if(confirm("Are you sure you want to quit? \n\n[My script might fail to close the tab though... \n(Browser security, blah blah)\n Why don't you just close the tab yourself, hm?]")){
+    if(confirm("Are you sure you want to quit? \n\n My script might fail to close the tab though... \n(Browser security, blah blah)\n\n Why don't you just close the tab yourself, hm?\n")){
         window.close();
     }
 }
