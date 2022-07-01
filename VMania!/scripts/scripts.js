e6b91ecaccfa2f242857a730a1536b157d9dc100
Vijay Kabbur <53096977+VijayKabbur@users.noreply.github.com>
2022-07-01 14:40:53 +0530

    Added a fun Quit Game option

---
+++ VMania!/scripts/scripts.js
@@ -0,0 +1,5 @@
+function quit_game(){
+    if(confirm("Are you sure you want to quit? \n\n[My script might fail to close the tab though... \n(Browser security, blah blah)\n Why don't you just close the tab yourself, hm?]")){
+        window.close();
+    }
+}
